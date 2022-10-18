1fe2649a699619bdf21cfa1f03b48e0752695e3e
fjcalero <fjcalero@gmail.com>
2022-10-18 19:54:48 +0200

    Practica semana 05

--- 02_DATA_ANALYSIS/01_PYTHON_201/week05/your-solution-here/CiscoCalero_04_pra_clases.py
+++ 02_DATA_ANALYSIS/01_PYTHON_201/week05/your-solution-here/CiscoCalero_04_pra_clases.py
@@ -2,10 +2,22 @@ import utils
 
 gonzalez = utils.Tienda ("Hermanos González", "c/ Argumosa, 10. Madrid", 3, [12000, 6000, 32432])
 maricarmen = utils.Tienda ("Electro Mari Carmen", "Avenida Grande, 4. Bollullos de la Mitación", 8, [23176, 2134, 70375])
-cari = utils.Tienda ("Carimesticos", "Plaza de Enmedio, s/n. Teruel", 2, [6578, 1200, 19456])
+cari = utils.Tienda ("Carimesticos", "Avenida de Enmedio, s/n. Teruel", 2, [6578, 1200, 19456])
 
 print(cari.total_ventas())
 print(cari.media_ventas_empleado())
 print(cari.tienda_direccion())
 print(cari.ventas_ultimo_mes())
 print(cari.proyeccion_ventas (500))
+
+
+tiendas = [gonzalez, maricarmen, cari]
+
+total_ventas = 0
+for i in range(len(tiendas)):
+    total_ventas += tiendas[i].ventas_ultimo_mes()
+print("El total de ventas del último mes es de: {}".format (total_ventas))
+
+for i in range(len(tiendas)):
+    if "Avenida" in tiendas[i].direccion:
+        print("La tienda {} está en una avenida".format(tiendas[i].nombre))
--- 02_DATA_ANALYSIS/01_PYTHON_201/week05/your-solution-here/utils.py
+++ 02_DATA_ANALYSIS/01_PYTHON_201/week05/your-solution-here/utils.py
@@ -1,10 +1,29 @@
 class Tienda():
     '''
-    Doc String
-    xxxxxxxxx
+    Crea instancias de tipo tienda
+    Métodos:
+        total_ventas: Total ventas de cada tienda
+        media_ventas_empleado: Media de ventas de cada empleado por tienda
+        tienda_direccion: Datos básicos con nombre y dirección
+        ventas_ultimo_mes: Ventas realizadas en el último mes
+        proyeccion_ventas: Predicción ventas en función de una inversion dada
+
+    Atributos:
+        nombre: str
+        direccion: str
+        empleados: int
+        ventas: list
+    Atributos opcionales:
+        tipo: str -> Electrodomesticos
+        abierta: bool -> True
+
+    Date: 18/10/2022
+    Author: Cisco Calero
+
     '''
     def __init__(self, nombre, direccion, empleados, ventas, tipo="Electrodomésticos", abierta=True):
-        self.phone = tipo
+        # Hacemos globales los argumentos de creación de la instancia
+        self.tipo = tipo
         self.abierta = abierta
         self.nombre = nombre
         self.direccion = direccion
@@ -12,20 +31,26 @@ class Tienda():
         self.ventas = ventas
         print("Clase Tienda inicializada correctamente!")
 
+    # Método para el cálculo del total de ventas del último trimestre
     def total_ventas (self):
         total = sum([i for i in self.ventas])
         self.total = total
         return total
-    
+
+    # Método para el cálculo de ventas media por empleado
     def media_ventas_empleado (self):
         return (sum(i for i in self.ventas)/len(self.ventas))/self.empleados
 
+    # Método que devuelve el nombre y la dirección de una instancia tienda
     def tienda_direccion (self):
         return "{} está en: {}".format(self.nombre, self.direccion)
 
+    # Método que devuelve las ventas del último mes
     def ventas_ultimo_mes (self):
         return self.ventas[-1]
 
+    # Método que calcula la proyección de ventas en función d
+    # de la inversión que se hace en acciones de marketing
     def proyeccion_ventas (self, inversion):
         return self.total * 1.5 if inversion >= 1000 else self.total * 1.2
 
